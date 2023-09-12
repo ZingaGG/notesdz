55520bb80376dbddcd5431f90a59b5e0fb674c0d
ZingaG <trad_bar@mail.ru>
2023-09-12 11:20:14 +0300

    Note initializer

--- note.py
+++ note.py
@@ -1,10 +1,19 @@
 import datetime
 
-class note:
-    def __init__(self, id, title, text, time):
-        self.id = id
+class Note:
+    id = 1
+
+    def __init__(self, title, text):
+        self.id = Note.id
         self.title = title
         self.text = text
-        self.time = datetime.datetime.now() 
+        self.date = datetime.datetime.now() 
+        Note.id += 1
+    
+    def toString(self):
+        print("Id = ", self.id)
+        print("Title = ", self.title)
+        print("Text = ", self.text)
+        print("Date = ", self.date)
 
     
